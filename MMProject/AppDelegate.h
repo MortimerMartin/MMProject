d791207738bc66b05301c62b728c11de3cb12309
MortimerMartin <874607211@qq.com>
2018-05-13 17:49:40 +0800

    Initial Commit

---
+++ MMProject/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MMProject
+//
+//  Created by Mortimer on 2018/5/13.
+//  Copyright © 2018年 Mortimer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
